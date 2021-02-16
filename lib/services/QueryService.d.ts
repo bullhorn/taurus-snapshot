7c2b29ce9802e56de07708d09252e4174066da8d
Thibaut Lassalle <thibaut.lassalle@bullhorn.com>
2021-02-16 16:36:56 +0000

    27048f9 update libs

--- lib/services/QueryService.d.ts
+++ lib/services/QueryService.d.ts
@@ -9,27 +9,43 @@ export declare class QueryService<T> {
     http: AxiosInstance;
     meta: MetaService;
     records: any[];
-    parameters: any;
+    parameters: {
+        fields: any;
+        orderBy?: string[];
+        start: number;
+        count: number;
+        where?: string;
+        layout?: string;
+        sort?: any;
+        query?: any;
+    };
     protected _page: number;
     protected _endpoint: string;
     protected _lastResponse: BullhornListResponse<T>;
+    private readonly initialized;
     /**
      * constructor description
      * @param endpoint - Base Url for all relative http calls eg. 'query/JobOrder'
      */
-    constructor(entity: string);
-    endpoint: string;
-    readonly total: Promise<number>;
-    readonly snapshot: BullhornListResponse<T>;
-    fields(...args: any[]): this;
+    constructor(entity: string, callingIdentifier?: string);
+    initialize(callingIdentifier?: string): Promise<void>;
+    get endpoint(): string;
+    set endpoint(value: string);
+    get total(): Promise<number>;
+    get snapshot(): BullhornListResponse<T>;
+    fields(...args: string[][]): this;
     sort(...args: any[]): this;
-    where(value: any): this;
-    query(value: any): this;
+    where(value: {}): this;
+    query(value: string): this;
     count(value: number): this;
     page(value: number): this;
     nextpage(): Promise<BullhornListResponse<T>>;
     params(object: any): this;
     get(add: any): Promise<BullhornListResponse<T>>;
-    run(add: any): Promise<BullhornListResponse<T>>;
-    then(done: any, fail?: any): Promise<any>;
+    run(add: boolean): Promise<BullhornListResponse<T>>;
+    private recursiveQueryPull;
+    private shouldPullMoreRecords;
+    private getNext;
+    private httpGet;
+    then(done: any, fail?: any): Promise<BullhornListResponse<T>>;
 }
