6acfd657fc8279ee728b5f3984e8c209b859c47c
Thibaut <1559534+tbo47@users.noreply.github.com>
2021-02-16 19:36:23 +0000

    63d57e2 chore(libs): update dev libs

--- lib/core/EntityList.d.ts
+++ lib/core/EntityList.d.ts
@@ -1,8 +1,8 @@
-import { StatefulSubject } from './StatefulSubject';
-import { Entity } from './Entity';
-import { QueryService, SearchService } from '../services';
-import { EntityListOptions } from './EntityListOptions';
 import { EntityMessageBroker } from '../broker';
+import { QueryService, SearchService } from '../services';
+import { Entity } from './Entity';
+import { EntityListOptions } from './EntityListOptions';
+import { StatefulSubject } from './StatefulSubject';
 export declare type EntityListReference<T> = SearchService<T> | QueryService<T>;
 export declare type EntityOperation = number | Entity<any>;
 export interface ListResults {
@@ -19,15 +19,20 @@ export declare class EntityList<T> extends StatefulSubject<T[]> {
     private readonly $ref;
     private readonly $list;
     protected broker: EntityMessageBroker;
-    constructor(type: string, options?: EntityListOptions, state?: T[]);
-    protected getSearcher(type: string): EntityListReference<T>;
-    readonly lastAdded: Entity<T>;
-    readonly info: any;
+    constructor(type: string, options?: EntityListOptions, state?: T[], callingIdentifier?: string);
+    protected getSearcher(type: string, callingIdentifier?: string): EntityListReference<T>;
+    get lastAdded(): Entity<T>;
+    get info(): {
+        total: number;
+        messages: import("../types/Responses").BullhornMessage[];
+        start: string;
+        count: string;
+    };
     findById(pk: number): Entity<T>;
     push(item: T): Promise<any>;
     update(item: EntityOperation, value: T): Promise<any>;
     remove(item: EntityOperation): Promise<any>;
-    private _setUpObservable();
-    private _eventHook(eventType);
-    private _checkOperationCases(item, cases);
+    private _setUpObservable;
+    private _eventHook;
+    private _checkOperationCases;
 }
